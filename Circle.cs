c99d99957555b421bee6c1ebf665bbb632131bf3
Ashfaqul Awal <ashfaq.awal@gmail.com>
2023-11-07 05:37:48 +0100

    uploading codes

---
+++ Circle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GrundernaOOPLabFive
+{
+	public class Circle
+	{
+		//int _radius; //not using this, instead using 'private'. besides 'pi'
+						//should be a double variable, even we use it from
+						//MATH.PI. So there an error will be generated if we use
+						//both int and double to get a result.
+
+		private double radius; // declaring a property to store radius data
+		private double pi = 3.14; // declaring another property for 'pi' and
+                                  // assigning value in it.
+
+
+        public Circle(double radius) // Creating a constructor with radius as
+									 // a parameter. Keeping it public so that
+									 // it can be accessed from other classes
+		{
+			this.radius = radius; // here by using 'this', actually setting the
+									// circle's radius.
+		}
+
+		public double GetArea() // Creating a method to calculate the area of
+								// circle
+		{
+			return radius * radius * pi;
+		}
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GrundernaOOPLabFive;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Creating new objects of Circle class
+        Circle circleOne = new Circle(5);
+        Circle circleTwo = new Circle(6);
+
+        double areaOne = circleOne.GetArea();
+        Console.WriteLine("Circle area with radious 5: "+ areaOne);
+
+        double areaTwo = circleTwo.GetArea();
+        Console.WriteLine("Circle area with radious 6: " + areaTwo);
+
+        Console.ReadLine();
+    }
+}
+
